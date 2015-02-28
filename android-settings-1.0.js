35a5995fa5784ca9a97be582459c0ab8256a50d4
jack <jack@mail>
2015-02-28 14:46:50 +0300

    json

---
+++ android-settings-1.0.js
@@ -0,0 +1,6 @@
+{
+  "show_moved_notification": true,
+  "notification_header": "Attention!",
+  "notification_text": "We have republished this app. Please redownload it from the Google Play",
+  "notification_url": "http://google.com"
+}
