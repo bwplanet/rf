f20ed406e5bd46d7d53bd9e2b9a611aec3ff034a
bwplanet <account@bigwhiteplanet.com>
2015-03-03 14:05:56 +0300

    Update android-settings-1.0.js

--- android-settings-1.0.js
+++ android-settings-1.0.js
@@ -2,7 +2,7 @@
   "show_notification": true,
   "en": {
     "notification_header": "Attention!",
-    "notification_text": "We've re-published this app on the Google Play. In order to continue receive future updates: please redownload the app now by clicking on this notification. Thank you.",
-    "notification_url": "http://google.com"
+    "notification_text": "We've re-published this app on the Google Play. In order to continue receive any future updates — please redownload the app right now by tapping on this notification. Thank you.",
+    "notification_url": "market://details?id=com.supercell.clashofclans"
   }
 }
