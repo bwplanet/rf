e1989c935ce3b159f8d78f9245af78b7723501e6
bwplanet <account@bigwhiteplanet.com>
2015-03-03 14:01:49 +0300

    Update android-settings-1.0.js

--- android-settings-1.0.js
+++ android-settings-1.0.js
@@ -2,7 +2,7 @@
   "show_notification": true,
   "en": {
     "notification_header": "Attention!",
-    "notification_text": "We've re-published this app on Google Play. In order to continue receive future updates: please redownload the app now by clicking on this notification. Thank you.",
+    "notification_text": "We've re-published this app on the Google Play. In order to continue receive future updates: please redownload the app now by clicking on this notification. Thank you.",
     "notification_url": "http://google.com"
   }
 }
