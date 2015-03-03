78c8fc26ae4c0a4bb6e4efe3b4f13cba1869354a
bwplanet <account@bigwhiteplanet.com>
2015-03-03 13:40:14 +0300

    Update android-settings-1.0.js

--- android-settings-1.0.js
+++ android-settings-1.0.js
@@ -1,6 +1,6 @@
 {
   "show_notification": true,
   "notification_header": "Attention!",
-  "notification_text": "We have republished this app. Please redownload it from the Google Play",
+  "notification_text": "We have republished this app.\nPlease redownload it from the Google Play",
   "notification_url": "http://google.com"
 }
