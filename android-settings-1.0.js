904c0bbddd8c44c45a6dbdf9b11476ac574c8ba1
bwplanet <account@bigwhiteplanet.com>
2015-05-04 16:59:37 +0300

    Update android-settings-1.0.js

--- android-settings-1.0.js
+++ android-settings-1.0.js
@@ -5,5 +5,5 @@
     "notification_header": "Attention!",
     "notification_text": "We've re-published this app on the Google Play. In order to continue receive any future updates — please redownload the app right now by tapping on this notification. Thank you."
   },
-  "appturbo_enabled": false
+  "appturbo_enabled": true
 }
